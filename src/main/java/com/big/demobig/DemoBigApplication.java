0c0a90142aded78538e4014cb7ec7a8f42b5c448
marcelaueno <marcelaueno@gmail.com>
2021-05-19 19:27:29 -0300

    first commit

---
+++ src/main/java/com/big/demobig/DemoBigApplication.java
@@ -0,0 +1,13 @@
+package com.big.demobig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/big/demobig/DemoBigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big.demobig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
